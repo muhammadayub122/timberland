a8d2f36e9b9a329e031eb3b03d5cb4f57feee663
MusulmanM <mmmmmmvvv200@gmail.com>
2026-02-06 18:28:48 +0500

    crud finished

--- order/views.py
+++ order/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .serializer import OrderSerializer,  AddressSerializer, OrderProductSerializer 
+from .models import Order, OrderProduct, Address
 # Create your views here.
+
+
+
+
+class OrderApiiview(ModelViewSet):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
+
+
+
+
+class OrderProductApiiview(ModelViewSet):
+    serializer_class = OrderProductSerializer
+    queryset = OrderProduct.objects.all()
+
+
+
+
+
+class AddressApiiview(ModelViewSet):
+    serializer_class = AddressSerializer
+    queryset = Address.objects.all()
+
