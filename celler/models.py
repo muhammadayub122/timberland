d5f6acb019435e892672742c50cfee7ad23d235e
MusulmanM <mmmmmmvvv200@gmail.com>
2026-02-25 14:36:58 +0500

    full code

--- celler/models.py
+++ celler/models.py
@@ -1,17 +1,11 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
-from django.db import models
-from django.contrib.auth.models import User
 from django.utils import timezone
 
 
 class Seller(models.Model):
-    user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE,
-        related_name='seller_profile'
-    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, blank=True)
     phone = models.CharField(max_length=50, blank=True)
     latitude = models.FloatField(null=True, blank=True)   
--- user/models.py
+++ user/models.py
@@ -1,23 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
-
-
-
-
-
-class User(models.Model):
+class User(AbstractUser):
     class Role(models.TextChoices):
         USER = 'USER', 'user'
         ADMIN = 'ADMIN', 'admin'
         CELLER = 'CELLER', 'celler'
-    first_name = models.CharField(max_length=155)
-    last_name = models.CharField(max_length=155)
-    middel_name = models.CharField(max_length=155)
-    user_name = models.CharField(max_length=155)
+    
+    
+    middel_name = models.CharField(max_length=155, blank=True, null=True)
     age = models.PositiveIntegerField(default=1)
     phone_number = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='user/',blank=True,null=True)
-    address = models.CharField(max_length=255)
-
-
+    image = models.ImageField(upload_to='user/', blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    role = models.CharField(max_length=10, choices=Role.choices, default=Role.USER)
+    
+    REQUIRED_FIELDS = ['email', 'phone_number']
+    
+    def __str__(self):
+        return f"{self.username} - {self.get_role_display()}"
